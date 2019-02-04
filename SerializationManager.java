7e87a6f94a13007bd5d3072245585489689abb60
Rahul <rahuljaiswal1808@users.noreply.github.com>
2019-02-04 18:57:08 +0530

    Update SerializationManager.java

--- SerializationManager.java
+++ SerializationManager.java
@@ -6,14 +6,18 @@ public class SerializationManager {
     public static <T extends Serializable> void serialize(T object, String key, Class<T> type) {
         Gson gson = new Gson();
         String jsonString = gson.toJson(object, type);
-        YourApp.getContext().getSharedPreferences(sharedPrefenceName, Context.MODE_PRIVATE).edit().putString(key, jsonString).commit();
+        SharedPrefUtil.setStringPref(key, jsonString);
     }
 
     public static <T extends Serializable> T deserialize(String key, Class<T> type) {
         T objectToReturn = null;
         Gson gson = new Gson();
-        String jsonString = YourApp.getContext().getSharedPreferences(sharedPrefenceName, Context.MODE_PRIVATE).getString(key, "");
+        String jsonString = SharedPrefUtil.getStringPref(key, "");
         objectToReturn = gson.fromJson(jsonString, type);
         return objectToReturn;
+    }
+    
+    public static void clear(String key) {
+        SharedPrefUtil.clear();
     }
 }
