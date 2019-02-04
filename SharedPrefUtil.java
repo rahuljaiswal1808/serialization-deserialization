6ff716138ad043d1d60e7b332423b1f13d449449
Rahul <rahuljaiswal1808@users.noreply.github.com>
2019-02-04 19:21:13 +0530

    Update SharedPrefUtil.java

--- SharedPrefUtil.java
+++ SharedPrefUtil.java
@@ -25,6 +25,42 @@ public class SharedPrefUtil {
     public static boolean getBoolPref(String key, boolean defVal) {
         return getSharedPref().getBoolean(key, defVal);
     }
+    
+    public static void setIntPref(String key, int value) {
+        getEditor().putInt(key, value).commit();
+    }
+
+    public static int getIntPref(String key, int defVal) {
+        return getSharedPref().getInt(key, defVal);
+    }
+
+    public static void setFloatPref(String key, float value) {
+        getEditor().putFloat(key, value).commit();
+    }
+
+    public static float getFloatPref(String key, float defVal) {
+        return getSharedPref().getFloat(key, defVal);
+    }
+
+    public static void setLongPref(String key, long value) {
+        getEditor().putLong(key, value).commit();
+    }
+
+    public static float getLongPref(String key, long defVal) {
+        return getSharedPref().getLong(key, defVal);
+    }
+
+    public static void setStringSetPref(String key,  Set<String> value) {
+        getEditor().putStringSet(key, value).commit();
+    }
+
+    public static Set<String> getStringSetPref(String key, Set<String> defVal) {
+        return getSharedPref().getStringSet(key, defVal);
+    }
+
+    public Map<String, ?> getAll() {
+        return getSharedPref().getAll();
+    }
 
     public static void clear() {
         getEditor().clear().commit();
